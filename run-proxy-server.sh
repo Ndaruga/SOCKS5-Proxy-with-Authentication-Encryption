364a370973ae744aedb80c4620aa107597ab8b2b
Francis Ndaruga <ndaruga.francis@gmail.com>
2025-02-28 22:42:35 -0500

    Clear install info

--- run-proxy-server.sh
+++ run-proxy-server.sh
@@ -3,5 +3,6 @@ python3 -m venv venv
 source venv/bin/activate
 echo Installing Libraries... ✅
 pip install --no-cache-dir -r requirements.txt
+clear
 echo Starting proxy server... ✅
 python3 proxy_server.py
